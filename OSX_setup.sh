bd2b0be6f344c2caaf53e311ef366cb0e67fc35e
mannerisms <mannerisms@gmail.com>
2015-08-18 11:22:05 +0200

    created single install script for OSX

---
+++ OSX_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#===============================================================================
+# File: vim_setup.sh
+# Date Created: 2015-07-30
+# Description:  Setup OSX to include all the dot files
+#
+#===============================================================================
+# Exit Codes:
+# 0) Succesful completetion
+#===============================================================================
+
+FILES=("vim" "slate" "tmux.conf" "vimrc")
+
+# Remove all existing dotfiles
+for FILE in ${FILES[@]}; do
+    OLDFILE="$HOME/.$FILE"
+    if [[ -e $OLDFILE ]]; then
+        echo "removing $OLDFILE"
+        rm -r $OLDFILE
+    fi
+    # Create symlink from file to new file
+    NEWFILE=$(pwd)"/$FILE"
+    echo "replacing $NEWFILE"
+    ln -s $NEWFILE $OLDFILE
+done
+
+echo "Changes sucessfully applied"
+exit 0
