a06665f5455f1d188a93d07655aae33b1ea12b28
mannerisms <mannerisms@gmail.com>
2018-12-28 11:40:42 +0100

    update for Linux

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
 # PATH
@@ -9,6 +11,12 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/loca
 # Set locale
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+
+# Set CAPS as CTRL when held
+/usr/bin/setxkbmap -option 'caps:ctrl_modifier'
+
+# Set vim as the default editor
+VISUAL=vim; export VISUAL EDITOR=vim; export EDITOR
 
 # Check for presence of virtualenvwrapper and source if available
 if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
@@ -21,7 +29,6 @@ if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
         export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
 fi
 
-
 # FileSearch
 function f() { find . -iname "*$1*" ${@:2} }
 function r() { grep "$1" ${@:2} -R . }
@@ -31,8 +38,9 @@ function mkcd() { mkdir -p "$@" && cd "$_"; }
 
 # ALIASES
 # Use sublimetext for editing config files
-alias zshconfig="subl ~/.zshrc"
-alias envconfig="subl ~/.env.sh"
+alias zshconfig="vim ~/.zshrc"
+alias envconfig="vim ~/.env.sh"
 alias g="git "
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
