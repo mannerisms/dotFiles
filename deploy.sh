18477e6e5fda75552e2404ae5ac108c5155b91c2
Bastiaan van der Laaken <mannerisms@gmail.com>
2019-01-05 02:40:55 +0100

    fixed update on OSX

--- deploy.sh
+++ deploy.sh
@@ -92,9 +92,9 @@ else
 	echo -e "\nNot backing up old dotfiles."
 fi
 
-printf "source '$HOME/dotfiles/zsh/zshrc_manager.sh'" > ~/.zshrc
-printf "so $HOME/dotfiles/vim/vimrc.vim" > ~/.vimrc
-printf "source-file $HOME/dotfiles/tmux/tmux.conf" > ~/.tmux.conf
+printf "source '$HOME/dotfiles/zsh/zshrc_manager.sh'" > $HOME/.zshrc
+printf "so $HOME/dotfiles/vim/vimrc.vim" > $HOME/.vimrc
+printf "source-file $HOME/dotfiles/tmux/tmux.conf" > $HOME/.tmux.conf
 
 # move config files for applications that exist in the config folder
 for filename in $HOME/dotfiles/config/*; do
