2021717a3b9dfe59f4d56c579f20c8a3f80cd6f9
Bastiaan van der Laaken <mannerisms@gmail.com>
2021-08-25 12:12:51 +0200

    -a

--- env/env.sh
+++ env/env.sh
@@ -3,8 +3,6 @@ source $ZSH/oh-my-zsh.sh
 
 # PATH
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/git/bin:/usr/local/mysql/bin"
-# export PYTHONPATH=$PYTHONPATH
-# export MANPATH="/usr/local/man:$MANPATH"
 
 # Set locale
 export LC_ALL=en_US.UTF-8
@@ -12,17 +10,6 @@ export LANG=en_US.UTF-8
 
 # Set vim as the default editor
 VISUAL=vim; export VISUAL EDITOR=vim; export EDITOR
-
-# Check for presence of virtualenvwrapper and source if available
-if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
-    source /usr/local/bin/virtualenvwrapper.sh
-    # Virtual Environment
-    export WORKON_HOME=$HOME/.virtualenvs
-    export PROJECT_HOME=$HOME/Development
-    export PIP_RESPECT_VIRTUALENV=true
-    # ensure all new environments are isolated from the site-packages directory
-    export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
-fi
 
 # switch caps and escape permanently
 if [[ -f /usr/bin/setxbmap ]]; then
@@ -48,4 +35,8 @@ alias gs="git status"
 # open multiple files in vim as tabs
 alias vim="vim -p"
 
+# set python path
+alias python=/usr/local/bin/python3
+
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+alias pip=/usr/local/bin/pip3
