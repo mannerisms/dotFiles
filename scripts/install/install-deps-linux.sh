094db6a7b40dfd3410b3e0a39abccbe141527e74
Bastiaan van der Laaken <mannerisms@gmail.com>
2024-02-09 22:57:25 +0100

    updated linux install script

--- scripts/install/install-deps-linux.sh
+++ scripts/install/install-deps-linux.sh
@@ -1,6 +1,11 @@
+apt-get update && apt-get upgrade
 apt-get install the_silver_searcher
 apt-get install tree
-apt-get install youtube-dl
+apt-get install yt-dlp
 apt-get install jsonpp
 apt-get install nvm
 apt-get install exa
+apt-get install bat
+apt-get install stow
+apt-get install alacritty
+apt-get install zsh
