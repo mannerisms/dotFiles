6a9d0e1aae3a93a1e037fff9a3ca0c86b97c307f
Bastiaan van der Laaken <mannerisms@gmail.com>
2024-02-09 21:55:06 +0100

    extracted bindkeys and added gitconfig

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -9,3 +9,13 @@ function r() { grep "$1" ${@:2} -r . }
 
 # mkdir and cd
 function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+function unmount_all {
+    diskutil list |
+    grep external |
+    cut -d ' ' -f 1 |
+    while read file
+    do
+        diskutil unmountDisk "$file"
+    done
+}
