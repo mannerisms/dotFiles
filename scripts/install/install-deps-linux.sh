e47003917fba79f0732f1827212ad0c44cce474b
Bastiaan van der Laaken <mannerisms@gmail.com>
2024-02-10 16:02:18 +0100

    made zsh install on linux optional

--- scripts/install/install-deps-linux.sh
+++ scripts/install/install-deps-linux.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
-apt-get install the_silver_searcher
 apt-get install tree
 apt-get install yt-dlp
-apt-get install jsonpp
 apt-get install nvm
 apt-get install bat
 apt-get install stow
-apt-get install zsh
 apt-get install trash
-apt-get install zsh-syntax-highlighting
-apt-get install zsh-autosuggestions
-apt-get install zsh-you-should-use
+apt-get install fzf
+
+# ask user if they want to install zsh
+read -p "Do you want to install zsh? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Installing zsh"
+    apt-get install zsh
+    apt-get install zsh-syntax-highlighting
+    apt-get install zsh-autosuggestions
+    apt-get install zsh-you-should-use
+else
+    echo "Skipping zsh installation"
+fi
