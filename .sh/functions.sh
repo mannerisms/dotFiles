4f41c9328711e387d9a3d5b32db3f8e6be50b745
Bastiaan van der Laaken <mannerisms@gmail.com>
2024-02-10 12:37:15 +0100

    fixed the functions

--- .sh/functions.sh
+++ .sh/functions.sh
@@ -1,16 +1,6 @@
 # FileSearch
-function f() { find . -iname "*$1*" ${@:2} }
-function r() { grep "$1" ${@:2} -r . }
+function f() { find . -iname "*$1*" ${@:2}; }
+function r() { grep "$1" ${@:2} -r .; }
 
 # mkdir and cd
 function mkcd() { mkdir -p "$@" && cd "$_"; }
-
-function unmount_all {
-    diskutil list |
-    grep external |
-    cut -d ' ' -f 1 |
-    while read file
-    do
-        diskutil unmountDisk "$file"
-    done
-}
--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -2,3 +2,13 @@
 function colormap() {
   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
+
+function unmount_all {
+    diskutil list |
+    grep external |
+    cut -d ' ' -f 1 |
+    while read file
+    do
+        diskutil unmountDisk "$file"
+    done
+}
